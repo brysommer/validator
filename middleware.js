818711274eaed3ff5db19b06c3eab2c6d95bb362
Roman <brysommer1@gmail.com>
2022-09-23 16:15:03 +0300

    commit

--- middleware.js
+++ middleware.js
@@ -5,13 +5,14 @@ const ajv = new Ajv();
 const mw = schema => (req, res, next) => {
     const validate = ajv.compile(schema);
     const valid = validate(req.body);
-    console.log(valid);
-    if (!valid) console.log(validate.errors);
     if (valid) {
         console.log('success');
         next();
         return;
     };
+
+    res.statusCode = 400;
+    res.json({ status :'Wrong data' });
 };
 
 module.exports = mw;
